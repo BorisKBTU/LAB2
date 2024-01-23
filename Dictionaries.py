34262c5087a75c9b793becc9ff6b448e685186c7
BorisKBTU <156778662+BorisKBTU@users.noreply.github.com>
2024-01-23 13:42:57 +0600

    Add files via upload

---
+++ Dictionaries.py
@@ -0,0 +1,12 @@
+# Section DICTIONARIES
+# Task1
+car = {"brand": "Ford", "model": "Mustang", "year": 1964}
+print(car.get("model"))
+# Task2
+car["year"] = 2020
+# Task3
+car["color"] = 'red'
+# Task4
+car.pop("model")
+# Task5
+car.clear()
